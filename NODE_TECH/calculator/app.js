a4960142dc1eeaa1b8d453727c33174a09b4cbad
Sakshi Tiwri <sakchitiwari5817@gmail.com>
2025-07-14 00:05:38 +0530

    Initial Commit node tutorials 5

---
+++ NODE_TECH/calculator/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const {userRequestHandler} = require('./handler');
+
+const server  = http.createServer(userRequestHandler);
+
+
+// alert('hi');
+
+const PORT = 3001;
+server.listen(PORT , ()=>{
+    console.log(`Server has started at port PORT ${PORT}`);
+
+
+})
---
+++ NODE_TECH/calculator/handler.js
@@ -0,0 +1,70 @@
+const userRequestHandler = (req, res) => {
+    console.log(req.url, req.method);
+    if (req.url === '/') 
+    {
+        res.setHeader('Content-Type', 'text/html');
+        res.write("<html>");
+        res.write('<head> <title>Practising my way</title></head> ');
+        res.write('<body> ');
+        res.write("<h1> Hi wonderful Peoples");
+        res.write('<br>');
+        res.write('<h1>Welcome </h1>');
+        res.write("<a href='/calculator'> Go To Calculaotro by clicleking here</a>");
+
+        // res.write();
+        res.write('</body>');
+
+        res.write("</html>");
+
+        return res.end();
+
+
+
+    } else if (req.url.toLowerCase() === '/calculator'){
+         res.setHeader('Content-Type', 'text/html');
+        res.write("<html>");
+        res.write('<head> <title>Practising my way</title></head> ');
+        res.write('<body> ');
+        res.write("<h1> Hi wonderful Peoples");
+        
+        // res.write();
+
+        res.write("Here is the calculator");
+        res.write("form  action = '/result'");
+        res.write("<input type = 'text' placeholder = 'FirstNum' name ='first'> firstnumber </input>");
+        res.write("<input type = 'text' placeholder = SecondNum' name ='second'> secondnum </input>");
+        res.write("</form>");
+        res.write('</body>');
+        res.write("<h1> Hello World </h1>");
+        res.write("</html>");
+        
+        
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+        res.write(`
+    <html>
+    <head><title> practise calculator </title></head>
+    <body>
+    <h1> 404 Not Found </h1>
+    <a href= " /" > Go to home</a>;
+
+    </body>
+    </html> ` );
+
+    res.end();
+}
+       
+
+
+
+
+
+
+
+
+
+
+
+
+exports.userRequestHandler = userRequestHandler;
